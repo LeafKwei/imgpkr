d77110c853c6af6bdb7bc7b358476ce2a17e3ddd
Leaf Kwei <leaf.kwei@outlook.com>
2025-07-23 10:54:40 +0800

    First commit.

---
+++ build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_imgpkr_FILE D:/K_File/K_Project/qt/imgpkr/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/imgpkr.exe)
+set(__QT_DEPLOY_TARGET_imgpkr_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_imgpkr_RUNTIME_DLLS D:/K_Application/K_InstallSoftware/QT/6.8.0/mingw_64/bin/Qt6Widgets.dll;D:/K_Application/K_InstallSoftware/QT/6.8.0/mingw_64/bin/Qt6Gui.dll;D:/K_Application/K_InstallSoftware/QT/6.8.0/mingw_64/bin/Qt6Core.dll)
